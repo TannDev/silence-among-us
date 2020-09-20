d3959eaded3273894c570f2b473ce81735d800e2
James Tanner <jftanner@us.ibm.com>
2020-09-20 15:15:21 -0400

    Chore: Reorganize how the schema is loaded

--- server.js
+++ server.js
@@ -1,26 +1,18 @@
 const createError = require('http-errors');
 const express = require('express');
 const swaggerUI = require('swagger-ui-express');
-const schemaParser = require('@apidevtools/json-schema-ref-parser');
 
 // Initialize the server
 const app = express();
 
 // Serve swagger UI
-const apiSpecPromise = schemaParser.bundle('./schemas/api.yaml');
 app.use(swaggerUI.serve);
-app.use('/',
-    (req, res, next) => {
-        apiSpecPromise
-            .then(schema => {
-                req.swaggerDoc = schema;
-                next();
-            })
-            .catch(error => next(createError(500, error)));
-    },
-    swaggerUI.setup()
-);
+app.get('/', swaggerUI.setup(require('./lib/loadSchema')));
 
+// Fail on anything else.
+app.use((req, res, next) => {
+    next(createError(404));
+})
 
 // Listen
 const server = app.listen(process.env.PORT || 3000, (error) => {
