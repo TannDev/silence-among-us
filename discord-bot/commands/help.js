d151756a3414aa3223c122af5a8dc4e93ef87b7f
James Tanner <jftanner@us.ibm.com>
2020-10-10 17:22:00 -0400

    Update: Much better help (Fixes #2)

--- discord-bot/commands/help.js
+++ discord-bot/commands/help.js
@@ -2,17 +2,31 @@ const { MessageEmbed } = require('discord.js');
 
 const instructionsUrl = 'https://github.com/tanndev/silence-among-us#readme';
 
-const commands = [
-    '`version|v`: Get version information.',
-    '`help|h|?`: Get this help.',
-    '`join|j [in-game name]`: Join the lobby as a player. Your in-game name will be stored for use next time.',
+const importantCommands = [
+    '`lobby|l start `: Start a new lobby.',
+    '`join|j [in-game name]`: Join the lobby as a player.',
     '`quit|q`: Quit the lobby and stop being a player.',
-    '`eject|e`: Eject the at-mentioned players from the lobby, as if they quit.',
-    '`lobby|l`: Re-post information about your lobby.',
-    '`lobby|l start [room code] [na|eu|asia]`: Start a new lobby.',
     '`lobby|l stop`: End your lobby.',
+].map(command => `\t- ${command}`).join('\n');
+
+const importantExamples = [
+    '`!sau lobby start`: Start a new lobby.',
+    '`!sau join`: Join the current lobby using your previously-saved in-game name.',
+    '`!sau join Alice`: Set your in-game name to "Alice" and join the current lobby.',
+    '`!sj Alice`: Same thing, but shorter.'
+].map(command => `\t- ${command}`).join('\n');
+
+const moreCommands = [
+    '`help|h|?`: Get this help.',
+    '`eject|e`: Eject the at-mentioned players from the lobby, as if they quit.',
+    '`lobby|l`: Re-post the info about your lobby.',
+    '`bot|version|v`: Get information about the bot, including the deployed version.'
+].map(command => `\t- ${command}`).join('\n');
+
+const manualCommands = [
+    '`lobby|l start [room code] [na|eu|asia]`: Start a new lobby with a room code.',
     '`lobby|l room|r <room code> [na|eu|asia]`: Update the room information of your lobby.',
-    '`lobby|l room|r unlist|remove|x`: Remove the room information of your lobby.',
+    '`lobby|l room|r unlist|remove|x`: Remove the room code for your lobby.',
     '`intermission|i`: Transition your lobby to the "intermission" phase.',
     '`working|work|w`: Transition your lobby to the "working" phase.',
     '`meeting|meet|m`: Transition your lobby to the "meeting" phase.',
@@ -20,21 +34,28 @@ const commands = [
     '`revive <@mentions...>`: Mark any at-mentioned players as being alive.'
 ].map(command => `\t- ${command}`).join('\n');
 
-const examples = [
-    '`!sau lobby start`: Start a new lobby with no room code.',
+const moreExamples = [
     '`!sl start abcdef eu`: Start a new lobby with a room code.',
     '`!sau work`: Start a new game, from intermission, or end a meeting.',
     '`!sw`: Same thing as `!sau work`, but shorter.',
     '`!sk me @tanner`: Mark yourself and @tanner as dead. (Must be a real at-mention)'
 ].map(command => `\t- ${command}`).join('\n');
 
-const helpEmbed = new MessageEmbed()
-    .setTitle("Silence Among Us - Help")
-    .setURL(instructionsUrl)
-    .setDescription("Use `!sau <command>` or `!s<command>` to tell me what to do.\nI'll use your current voice channel to find your lobby.")
-    .addField('Available Commands', `${commands}\n\nSee the [instructions](${instructionsUrl}) for more details.`)
-    .addField('Examples', examples);
 
-module.exports = async function helpCommand(message) {
-    return message.channel.send(helpEmbed);
+module.exports = async function helpCommand(message, arguments) {
+    const embed = new MessageEmbed()
+        .setTitle("Silence Among Us - Help")
+        .setURL(instructionsUrl)
+        .setDescription("Use `!sau <command>` or `!s<command>` to tell me what to do.\nI'll use your current voice channel to find your lobby.")
+        .addField('Important Commands', importantCommands)
+
+    if (arguments[0] === 'more') embed
+        .addField('Other Useful Commands', moreCommands)
+        .addField('Manually Controlling Games', manualCommands)
+        .addField('Examples', moreExamples)
+    else embed
+        .addField('Examples', importantExamples)
+        .addField('Get More Help', 'Use `!sau help more` for all the commands,\nincluding how to run games manually.')
+
+    return message.channel.send(embed);
 };
