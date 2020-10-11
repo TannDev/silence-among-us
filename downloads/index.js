d75a55d82809aaee7bf0bc20a57a126f079716da
James Tanner <jftanner@us.ibm.com>
2020-10-10 21:23:34 -0400

    Upgrade: AmongUsCapture to 2.3.2

--- downloads/index.js
+++ downloads/index.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const version = '2.3.1';
+const version = '2.3.2';
 const publicRelease = Boolean(version.match(/^\d+\.\d+\.\d+$/));
 const filepath = path.resolve(__dirname, `AmongUsCapture-${version}.exe`);
 const filename = `AmongUsCapture-${version}.exe`;
