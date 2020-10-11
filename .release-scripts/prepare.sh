8106a0299e580597f103d0c0b50bab8b8d20ee8b
James Tanner <jftanner@us.ibm.com>
2020-10-11 00:10:30 -0400

    Build: Remove useless caches
    Because we include the version in package.json, this always invalidates our cache.

--- .release-scripts/prepare.sh
+++ .release-scripts/prepare.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -e
 # Build a release image to pick up the new version.
-docker pull "${IMAGE}:latest" || true
-docker pull "${IMAGE}:beta" || true
-docker build . --tag release-image --cache-from "${IMAGE}:latest" --cache-from "${IMAGE}:beta"
+docker build . --tag release-image
 
 # Log into container registry
 echo "${GITHUB_TOKEN}" | docker login https://ghcr.io -u "${GITHUB_USER}" --password-stdin
