4312ec01c65fc98800105e53cb7c1b41df22c1b3
James Tanner <jftanner@us.ibm.com>
2020-10-10 22:53:07 -0400

    Build: Remove unnecessary pipes

--- .release-scripts/prepare.sh
+++ .release-scripts/prepare.sh
@@ -2,7 +2,7 @@
 # Build a release image to pick up the new version.
 docker pull "${IMAGE}:latest" || true
 docker pull "${IMAGE}:beta" || true
-docker build . --tag release-image --cache-from "${IMAGE}:latest" --cache-from "${IMAGE}:beta" > /dev/null
+docker build . --tag release-image --cache-from "${IMAGE}:latest" --cache-from "${IMAGE}:beta"
 
 # Log into container registry
-echo "${GITHUB_TOKEN}" | docker login https://ghcr.io -u "${GITHUB_USER}" --password-stdin > /dev/null
+echo "${GITHUB_TOKEN}" | docker login https://ghcr.io -u "${GITHUB_USER}" --password-stdin
