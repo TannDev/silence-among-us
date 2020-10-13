9287337fe72af0d0954679f1e8e4be7821979e39
James Tanner <james.tanner@tanndev.com>
2020-10-13 19:49:27 -0400

    Update: Capture => SFX Prerelease
    Now with the fantastic UI overhaul from @CarbonNeuron

--- downloads/index.js
+++ downloads/index.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const version = '2.3.4';
+const version = 'SFX';
 const publicRelease = Boolean(version.match(/^\d+\.\d+\.\d+$/));
 const filepath = path.resolve(__dirname, `AmongUsCapture-${version}.exe`);
 const filename = `AmongUsCapture-${version}.exe`;
