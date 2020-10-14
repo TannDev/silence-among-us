cfa77ba5d9c5d7a86e151b534bb08e29a8cfe4a5
James Tanner <james.tanner@tanndev.com>
2020-10-13 20:19:27 -0400

    Upgrade: Capture => R2R Prelease

--- downloads/index.js
+++ downloads/index.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const version = 'SFX';
+const version = 'SFX-R2R';
 const publicRelease = Boolean(version.match(/^\d+\.\d+\.\d+$/));
 const filepath = path.resolve(__dirname, `AmongUsCapture-${version}.exe`);
 const filename = `AmongUsCapture-${version}.exe`;
