bff225bc542cef7e96273c01735b753406a35880
James Tanner <james.tanner@tanndev.com>
2020-10-22 21:53:35 -0400

    Update: Remove deprecated APIs

--- api-server/api.js
+++ api-server/api.js
@@ -27,40 +27,6 @@ router.get('/server/lobbies', (req, res, next) => {
         .catch(error => next(error));
 })
 
-router.param('voiceChannelId', (req, res, next, voiceChannelId) => {
-    Lobby.findByVoiceChannel(voiceChannelId)
-        .then(lobby => {
-            if (lobby) {
-                req.lobby = lobby;
-                next();
-            }
-            else next(createError(404, "No lobby exists for that voice channel."));
-        })
-        .catch(error => next(error));
-});
-
-router.param('playerId', (req, res, next, playerId) => {
-    req.lobby.getDiscordPlayer(playerId)
-        .then(player => {
-            if (player) {
-                req.player = player;
-                next();
-            }
-            else next(createError(404, "No such player exists for that lobby."));
-        })
-        .catch(error => next(error));
-});
-
-router.get('/lobby/:voiceChannelId', (req, res) => {
-    res.json(req.lobby.toJSON());
-});
-
-router.get('/lobby/:voiceChannelId/:playerId/kill', (req, res, next) => {
-    req.lobby.guildMemberKill(req.player.guildMember)
-        .then(player => res.json(player))
-        .catch(error => next(error));
-});
-
 router.use((req, res, next) => {
     next(createError(404, "No such API endpoint."));
 });
